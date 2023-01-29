b11dd508396a0a69067efa9cc373aa37ac5de40e
Balasubrahmanyam P <pbs9948@gmail.com>
2023-01-29 12:20:25 +0530

    recipes app

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
+# Base image
 FROM python:3.9
 
+# Set environment varibles
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
-COPY requirements /app/requirements.txt
+COPY requirements.txt /app/requirements.txt
 
-RUN pip install -r requirements.txt
+RUN pip install -r /app/requirements.txt
 
 COPY . /app
 
-EXPOSE 5000
+EXPOSE 80
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -7,10 +7,12 @@ from app.domain.recipes import RecipesDomain
 from app.repository.recipes import RecipesRepository
 from app.routers.recipes import RecipesRouter
 
-
 app = FastAPI()
 
+
 db = initialize_db()
+
+
 recipes_repository = RecipesRepository(db)
 recipes_domain = RecipesDomain(recipes_repository)
 recipes_router = RecipesRouter(recipes_domain)
@@ -21,3 +23,6 @@ app.include_router(recipes_router.router)
 @app.get('/')
 def index():
     return 'Hello World!'
+
+if __name__ == '__main__':
+    uvicorn.run("app.main:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
